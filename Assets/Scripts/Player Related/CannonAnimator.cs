99bb1babeb429b8c4569bb1856f8387e1dd6fb82
DevCas1 <18188660+DevCas1@users.noreply.github.com>
2021-03-23 16:09:57 +0100

    Mostly finalize Arm Cannon bob

--- Assets/Scripts/Player Related/CannonAnimator.cs
+++ Assets/Scripts/Player Related/CannonAnimator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using DG.Tweening;
 using UnityEngine;
@@ -18,12 +19,15 @@ namespace AMPR
         private float PunchElasticity = 0.33f;
 
         [Header("Cannon bob Related")]
-        [SerializeField, Range(-1, 1)]
+        [SerializeField, /*Range(-1, 1)*/]
         private Vector2 BobMagnitudeMultiplier = new Vector2(0.5f, 0.5f);
+        [SerializeField, /*Range(-1, 1)*/]
+        private Vector2 SinusOffset = new Vector2(0.2f, 0);
         [SerializeField]
-        private float BobSpeed = 1;
+        private float BobSpeed = 4;
 
-        private const float SINOffset = 0.5f;
+        private const float SINOffset = 1;
+        private const float SINLength = 6.25f;
 
         private bool _punchActive;
         private Tweener _punchTweener;
@@ -33,23 +37,27 @@ namespace AMPR
         private Vector2 _bobInput;
         private bool _shouldBob;
         private bool _bobActive;
-        private float _currentSinT;
+        private bool _shouldReset;
+        private float _currentSinusT;
         private float _currentSinus;
         private IEnumerator _smoothBobReset;
+
+        private Vector3 LocalPos => transform.localPosition;
 
         private void Start()
         {
             InputHandler.Controls.Player.Fire.performed += context => OnCannonFire();
             InputHandler.Controls.Player.Move.performed += context => OnPlayerMove(context.ReadValue<Vector2>());
             InputHandler.Controls.Player.Move.canceled += context => OnPlayerStopMoving();
+
             DebugUtility.HandleErrorIfNullGetComponent<InputHandler, CannonAnimator>(InputHandler, this, gameObject);
 
-            _startPos = transform.localPosition;
+            _startPos = LocalPos;
         }
 
         private void Update()
         {
-            if (!_punchActive && (_shouldBob || _bobActive))
+            if (!_punchActive && (_bobActive || (_shouldBob && !_bobActive)))
             {
                 PerformCannonBob();
             }
@@ -57,14 +65,26 @@ namespace AMPR
 
         private void PerformCannonBob()
         {
-            _currentSinus = Mathf.Sin(_currentSinT);
+            _shouldBob = false;
+            _bobActive = true;
 
-            _bobDisplacement = new Vector3(_startPos.x + ((_currentSinus - SINOffset) * BobMagnitudeMultiplier.x),
-                                           _startPos.y + -(_currentSinus * BobMagnitudeMultiplier.y),
-                                           0);
+            if (_shouldReset)
+            {
 
-            _currentSinT += Time.deltaTime * BobSpeed;
-            transform.localPosition = Vector3.Lerp(transform.localPosition, _bobDisplacement, Mathf.SmoothStep(0, 1, _currentSinus));
+            }
+            else
+            {
+                _currentSinus = (float)Math.Sin(_currentSinusT);
+
+                _bobDisplacement = _startPos + new Vector3(_currentSinus * BobMagnitudeMultiplier.x,
+                                                                  -Mathf.Abs(_currentSinus) * BobMagnitudeMultiplier.y,
+                                                                  0);
+
+
+                _currentSinusT += Time.deltaTime * BobSpeed * _bobInput.magnitude;
+            }
+
+            transform.localPosition = Vector3.Lerp(LocalPos, _bobDisplacement, Time.deltaTime);
         }
 
         private void ResetBob(bool smoothly)
@@ -74,7 +94,7 @@ namespace AMPR
                 transform.localPosition = _startPos;
                 _shouldBob = false;
                 _bobActive = false;
-                _currentSinT = SINOffset;
+                _currentSinusT = SINOffset;
                 return;
             }
 
@@ -83,7 +103,21 @@ namespace AMPR
 
         private IEnumerator SmoothResetBob()
         {
-            return null;
+            transform.localPosition = Vector3.Lerp(transform.localPosition, _startPos, Time.deltaTime * BobSpeed);
+
+            if (Vector3.Distance(LocalPos, _startPos) < 0.05f)
+            {
+                transform.localPosition = _startPos;
+                _shouldBob = false;
+                _bobActive = false;
+                _currentSinusT = SINOffset;
+
+                StopCoroutine(_smoothBobReset);
+
+                _smoothBobReset = null;
+            }
+
+            yield return new WaitForEndOfFrame();
         }
 
         private void OnCannonFire()
@@ -96,22 +130,10 @@ namespace AMPR
             if (_punchActive)
                 _punchTweener.Complete(false);
 
-            TweenCallback SetPunchActive(bool active)
-            {
-                _punchActive = active;
-                return null;
-            }
-
-            TweenCallback RemoveReference()
-            {
-                _punchTweener = null;
-                return null;
-            }
-
             _punchTweener = transform.DOPunchPosition(new Vector3(0, 0, -PunchForce), PunchDuration, 0, PunchElasticity)
-                .OnStart(SetPunchActive(true))
-                .OnComplete(SetPunchActive(false))
-                .OnKill(RemoveReference());
+                .OnStart(() => _punchActive = true)
+                .OnComplete(() => _punchActive = false)
+                .OnKill(() => _punchTweener = null);
         }
 
         private void OnPlayerMove(Vector2 input)
@@ -120,6 +142,6 @@ namespace AMPR
             _shouldBob = true;
         }
 
-        private void OnPlayerStopMoving() => _bobActive = false;
+        private void OnPlayerStopMoving() => ResetBob(true);
     }
 }
