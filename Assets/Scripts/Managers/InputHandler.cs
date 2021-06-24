e2796ce8caa1bb79e5386bc95849358d35984d5c
DevCas1 <18188660+DevCas1@users.noreply.github.com>
2021-06-24 23:54:51 +0200

    Fix recurring compiler error at compilation | Import Cinemachine

--- Assets/Scripts/Managers/InputHandler.cs
+++ Assets/Scripts/Managers/InputHandler.cs
@@ -4,13 +4,22 @@ namespace AMPR.Manager
 {
     public class InputHandler : MonoBehaviour
     {
-        public PlayerControls Controls => _controls ??= new PlayerControls();
-        private PlayerControls _controls = new PlayerControls();
+        public PlayerControls Controls { get => _controls; }
+        private PlayerControls _controls;
+
+        private void Awake() => InitPlayerControls();
 
         private void Start() // TODO:Integrate Cursor Lockstate with Menu UI
         {
+            InitPlayerControls();
             Controls.Player.Cancel.performed += context => RemoveCursorLockState();
             Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        private void InitPlayerControls()
+        {
+            if (_controls == null)
+                _controls = new PlayerControls();
         }
 
         private static void RemoveCursorLockState() => Cursor.lockState = CursorLockMode.None;
